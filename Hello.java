c6ab4a9c5fd19653c02573526ce627ca17c5d6cd
bixben <bixonbeno337@gmail.com>
2024-03-04 11:16:35 +0530

    datatypes in java

--- Hello.java
+++ Hello.java
@@ -3,16 +3,27 @@ class Hello
 
     public static void main(String a[])
     {
-        int num1 = 3;
-        int num2 = 5;
-        int result;
-        System.out.println("Hello world");
-        System.out.println("This is java");
+        int num1=9;
+        byte by=127;
+        short sh=558;
+        long l=6745l;//always use l with long variables
+
+        float f=5.8f;//by default java only knows double ,so explicitly mention f
+        double d=5.8;
+
+        char c='b';
+
+        boolean b=true;
+
+        //printing everything
         System.out.println(num1);
-        System.out.println(num2);
-        result = num1 + num2;
-        System.out.println("The sum of the number is given below");
-        System.out.println(result);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+
 
 
     }
