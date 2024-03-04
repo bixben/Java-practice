0234817344e422a94b4c7039f0694e2e742e7d83
bixben <bixonbeno337@gmail.com>
2024-03-04 10:52:12 +0530

    intro to java

---
+++ Hello.java
@@ -0,0 +1,19 @@
+class Hello
+{
+
+    public static void main(String a[])
+    {
+        int num1 = 3;
+        int num2 = 5;
+        int result;
+        System.out.println("Hello world");
+        System.out.println("This is java");
+        System.out.println(num1);
+        System.out.println(num2);
+        result = num1 + num2;
+        System.out.println("The sum of the number is given below");
+        System.out.println(result);
+
+
+    }
+}
