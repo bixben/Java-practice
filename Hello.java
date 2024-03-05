94136da6b7122ec74bcdc3d522ab5307d980d1ef
bixben <bixonbeno337@gmail.com>
2024-03-05 11:27:24 +0530

    switch

--- Hello.java
+++ Hello.java
@@ -2,10 +2,33 @@ class Hello
 {
     public static void main(String args[])
     {
-        int c=9;
-        int result= ++c;
-        System.out.println(c);
-        System.out.println(result);
+        int c=10;
+        switch(c){
+            case 1:
+                System.out.println("Montag");
+                break;
+            case 2:
+                System.out.println("Dienstag");
+                break;
+            case 3:
+                System.out.println("Mittawoh");
+                break;
+            case 4:
+                System.out.println("Donnerstag");
+                break;
+            case 5:
+                System.out.println("Freitag");
+                break;
+            case 6:
+                System.out.println("Samstag");
+                break;
+            case 7:
+                System.out.println("Sontag");
+                break;
+            default:
+                System.out.println("Deine zahlen ist Falsch");
+
+        }
 
     }
 }
