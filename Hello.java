a530d76b124758c8e3303b424e05262167849f85
bixben <bixonbeno337@gmail.com>
2024-03-05 11:20:12 +0530

    increment and decremnt

--- Hello.java
+++ Hello.java
@@ -1,21 +1,11 @@
 class Hello
 {
-
     public static void main(String args[])
     {
-        //byte b= 127;
-        int a=257;
-        byte k=(byte)a;
-
-        float f=5.6f;
-        int t=(int)f;
-        System.out.println(t);
-
-        byte b=10;
-        byte c=25;
-        int result = b*c;//the type is changed to int because the result value goes out of the byte rage
+        int c=9;
+        int result= ++c;
+        System.out.println(c);
         System.out.println(result);
-
 
     }
 }
